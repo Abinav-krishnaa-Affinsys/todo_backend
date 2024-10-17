f5a55150ddf67c0ff9531a0a58b6e33b28d8c059
Abinav-krishnaa-Affinsys <abinav.krishnaa@affinsys.com>
2024-10-17 15:19:46 +0530

    aded todo  model

--- api/models.py
+++ api/models.py
@@ -11,4 +11,16 @@ class User(models.Model):
     def  __str__(self):
         return self.username
 
+
+class TodoModel(models.Model):
+
+    title = models.CharField(max_length=50 , primary_key= True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed  = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+     
+    def __str__(self):
+        return self.title
+    
 # Create your models here.
